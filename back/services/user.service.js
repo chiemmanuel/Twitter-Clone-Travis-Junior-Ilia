d7f820223f8c3cc7504c62e1c0800a702f2e5ea0
chiemmanuel <123269646+chiemmanuel@users.noreply.github.com>
2024-02-04 20:09:17 +0100

    functional signup

--- back/services/user.service.js
+++ back/services/user.service.js
@@ -1,13 +1,27 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const pool = require('../boot/database/mysql_db_connect'); 
+const pool = require('../boot/database/mysql_db_connect');
 
 // User signup
-const signup = async (req, res) => {
+const signup = (req, res) => {
     const { username, email, password, bio, gender, dob, contact } = req.body;
     let followers = 0;
     let following = 0;
     let created_at = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    // Parse the incoming date string
+    const parsedDate = new Date(dob);
+
+    // Check if the parsed date is a valid date
+    if (!isNaN(parsedDate)) {
+    // Format the date to 'YYYY-MM-DD'
+        const formattedDate = parsedDate.toISOString().split('T')[0];
+
+        // Now, `formattedDate` can be used to store in MySQL
+        console.log(formattedDate);
+    } else {
+        console.error("Invalid date format");
+    // Handle the case where the incoming date is invalid
+    }
 
     if (!username || !password || !email || !bio || !gender || !dob || !contact) {
         return res.status(400).json({ error: "Missing information" });
@@ -15,41 +29,47 @@ const signup = async (req, res) => {
 
     const hash = bcrypt.hashSync(password, 10);
 
-    try {
-        const connection = await pool.getConnection();
-
-        const [existingUsers] = await connection.query("SELECT * FROM users WHERE email = ?", [email]);
-        if (existingUsers.length > 0) {
-        connection.release();
-        return res.status(400).json({ message: "User with this email already exists" });
+    pool.query("SELECT * FROM users WHERE email = ?", [email], (err, existingUsers) => {
+        if (err) {
+            console.error("Error while checking existing users", err);
+            return res.status(500).json({ message: "Failed to check existing users" });
         }
-        await connection.query(
-            "INSERT INTO users (email, username, password, bio_qoute, gender, dob, contact, followers_count, following_count, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            [email, username, hash, bio, gender, dob, contact, followers, following, created_at]
-        );
-        connection.release();
 
-        return res.status(200).json({ message: "User registered successfully" });
-    } catch (error) {
-        console.error("Error while saving user to DB", error.message);
-        return res.status(500).json({ message: "Failed to save user" });
-    }
+        if (existingUsers && existingUsers.length > 0) {
+            return res.status(400).json({ message: "User with this email already exists" });
+        }
+
+        pool.query(
+            "INSERT INTO users (email, username, password, bio_quote, gender, dob, contact, followers_count, following_count, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            [email, username, hash, bio, gender, parsedDate, contact, followers, following, created_at],
+            (err) => {
+                if (err) {
+                    console.error("Error while saving user to DB", err);
+                    return res.status(500).json({ message: "Failed to save user" });
+                }
+
+                return res.status(200).json({ message: "User registered successfully" });
+            }
+        );
+    });
 };
 
 // User signin
-const signin = async (req, res) => {
+const signin = (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
         return res.status(400).json({ error: "Missing information" });
     }
 
-    try {
-        const connection = await pool.getConnection();
-        const [rows] = await connection.query("SELECT * FROM users WHERE email = ?", [email]);
-        connection.release();
+    pool.query("SELECT * FROM users WHERE email = ?", [email], (err, rows) => {
+        if (err) {
+            console.error("Error while getting user from DB", err);
+            return res.status(500).json({ error: "Failed to get user" });
+        }
 
         const user = rows[0];
+        console.log(user.email)
 
         if (!user) {
             return res.status(400).json({ message: "User not found" });
@@ -58,9 +78,14 @@ const signin = async (req, res) => {
         if (!bcrypt.compareSync(password, user.password)) {
             return res.status(400).json({ message: "Email or password don't match" });
         }
+        //create a session
+        req.session.user = {
+            email: user.email,
+          };
+        console.log(req.session.user);
 
         const token = jwt.sign(
-            { user: {email: user.email } },
+            { user: { email: user.email } },
             process.env.JWT_SECRET_KEY,
             {
                 expiresIn: "1h",
@@ -68,18 +93,14 @@ const signin = async (req, res) => {
         );
 
         return res.status(200).json({ token });
-    } catch (error) {
-        console.error("Error while getting user from DB", error.message);
-        return res.status(500).json({ error: "Failed to get user" });
-    }
+    });
 };
+
 const updateUser = async (req, res) => {
-    const { email } = req.user;
+    const { email } = req.session.user.email;
     const { username, bio, gender, dob, contact } = req.body;
 
     try {
-        const connection = await pool.getConnection();
-
         const updateValues = [];
         const updateFields = [];
 
@@ -112,9 +133,10 @@ const updateUser = async (req, res) => {
             return res.status(400).json({ error: "No fields to update" });
         }
 
-        await connection.query(`UPDATE users SET ${updateFields.map(field => `${field} = ?`).join(', ')} WHERE email = ?`, [...updateValues, email]);
-
-        connection.release();
+        pool.query(
+            `UPDATE users SET ${updateFields.map(field => `${field} = ?`).join(', ')} WHERE email = ?`,
+            [...updateValues, email]
+        );
 
         return res.status(200).json({ message: "User information updated successfully" });
     } catch (error) {
@@ -123,9 +145,8 @@ const updateUser = async (req, res) => {
     }
 };
 
-// Edit user password
 const updatePassword = async (req, res) => {
-    const { email } = req.user;
+    const { email } = req.session.user.email;
     const { oldPassword, newPassword } = req.body;
 
     if (!oldPassword || !newPassword) {
@@ -133,8 +154,7 @@ const updatePassword = async (req, res) => {
     }
 
     try {
-        const connection = await pool.getConnection();
-        const [rows] = await connection.query("SELECT * FROM users WHERE email = ?", [email]);
+        const [rows] = pool.query("SELECT * FROM users WHERE email = ?", [email]);
         const user = rows[0];
 
         if (!user) {
@@ -146,9 +166,7 @@ const updatePassword = async (req, res) => {
         }
 
         const hash = bcrypt.hashSync(newPassword, 10);
-        await connection.query("UPDATE users SET password = ? WHERE email = ?", [hash, email]);
-
-        connection.release();
+        pool.query("UPDATE users SET password = ? WHERE email = ?", [hash, email]);
 
         return res.status(200).json({ message: "Password updated successfully" });
     } catch (error) {
@@ -157,17 +175,21 @@ const updatePassword = async (req, res) => {
     }
 };
 
-
-// Get user info
 const getUser = async (req, res) => {
-    const { email } = req.user;
+    console.log(req.session.user);
+    console.log(req.user);
+    if (!req.session.user || typeof req.session.user !== 'object') {
+        return res.status(400).json({ message: "Invalid user object" });
+    }
+
+    const { email } = req.session.user.email;
+    console.log(email);
 
     try {
-        const connection = await pool.getConnection();
-        const [rows] = await connection.query("SELECT * FROM users WHERE email = ?", [email]);
-        connection.release();
+        const [rows] = pool.query("SELECT * FROM users WHERE email = ?", [email]);
 
         const user = rows[0];
+        console.log(user);
 
         if (!user) {
             return res.status(400).json({ message: "User not found" });
@@ -178,14 +200,11 @@ const getUser = async (req, res) => {
         console.error("Error while getting user from DB", error.message);
         return res.status(500).json({ error: "Failed to get user" });
     }
-};
+}
 
-//Get all users from the database
 const getUsers = async (req, res) => {
     try {
-        const connection = await pool.getConnection();
-        const [rows] = await connection.query("SELECT * FROM users");
-        connection.release();
+        const [rows] = pool.query("SELECT * FROM users");
 
         return res.status(200).json(rows);
     } catch (error) {
@@ -194,14 +213,13 @@ const getUsers = async (req, res) => {
     }
 };
 
-// Logout user and destroy token and session
 const logout = (req, res) => {
-        if (req.session.user) {
-            delete req.session.user;
-        }
-    
-        return res.status(200).json({ message: "Disconnected" });
-    };
+    if (req.session.user) {
+        delete req.session.user;
+    }
+
+    return res.status(200).json({ message: "Disconnected" });
+};
 
 module.exports = {
     signup,
