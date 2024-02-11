0b6595d57fb12bef0e952f681206c2a57a577583
ILIA TISKIN <tiskin2308@gmail.com>
2024-02-11 13:50:40 +0300

    Added compnents, pages and styles folders. Implemented basic routing logic in App.js and index.js.

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
