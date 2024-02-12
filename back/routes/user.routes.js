8a734e47039c62378dac5645e25cc6e954f03e11
chiemmanuel <123269646+chiemmanuel@users.noreply.github.com>
2024-02-12 23:57:46 +0100

    added some routes and services to ease front end tasks
    Added get user like tweets and delete route for the profile page

--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -8,9 +8,11 @@ router.put("/update", userServices.updateUser);
 router.put("/updatepassword", userServices.updatePassword);
 router.get("/get/:username", userServices.getUserByUsername);
 router.get("/getTweets/:user_email", userServices.getUserTweets);
+router.get("/getlikedTweets/:_id", userServices.getUserLikedTweets);
 router.get("/getComments/:username", userServices.getUserComments);
 router.get("/getme", userServices.getcurrentUser);
 router.post("/logout", userServices.logout);
+router.delete("/delete", userServices.deleteCurrentUser);
 
 
 module.exports = router;
